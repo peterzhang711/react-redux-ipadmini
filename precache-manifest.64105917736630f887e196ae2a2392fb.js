63492d0d9cf3f0f5608079e927974edec6b823dd
raben4@hotmail.com <raben4@hotmail.com>
2020-08-08 20:20:36 +1000

    Updates

---
+++ precache-manifest.64105917736630f887e196ae2a2392fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5a632c3b3b95f02c1a301e9b7bf1cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0407122eeedf5f69e112",
+    "url": "./static/css/main.b77fd48b.chunk.css"
+  },
+  {
+    "revision": "1fb39a96512f03f202b2",
+    "url": "./static/js/2.84777634.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.84777634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0407122eeedf5f69e112",
+    "url": "./static/js/main.6defdf45.chunk.js"
+  },
+  {
+    "revision": "887e6680fc7593a226e0",
+    "url": "./static/js/runtime-main.ece370c5.js"
+  },
+  {
+    "revision": "27ac1ac9347e654b3ea01d12dda5aafa",
+    "url": "./static/media/ipad-mini.27ac1ac9.jpg"
+  }
+]);
